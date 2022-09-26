f173f0db8e5b0bdc4baaee2754bdd1833e17d054
RL2020 <loc.fitness1@gmail.com>
2022-09-26 15:11:39 -0400

    hello world

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-const store = createStore(reducers, applyMiddleware(thunk));
+// const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <Provider  store={store}>,    
+    // <Provider  store={store}>,    
     <App />, 
-    </Provider>, 
+    // </Provider>, 
     document.querySelector('#root'));
