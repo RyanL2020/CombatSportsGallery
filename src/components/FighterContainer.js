0764a2213cf8fb67bdcf59944ac2e22dd5ec56e5
RL2020 <loc.fitness1@gmail.com>
2022-09-26 15:21:53 -0400

    fighter container

---
+++ src/components/FighterContainer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import FighterList from './BookList';
+
+class FighterContainer extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      fighters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/fighters')
+      .then(response => response.json())
+      .then(fighterData => this.setState({ fighters: fighterData.fighters }))
+  }
+
+  render() {
+    return <FighterList fighters={this.state.fighters} />
+  }
+}
+
+export default FighterContainer;
+
+
+
+
