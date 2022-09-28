9c8a3f18da0450695f3d963fdce2281b39d748aa
RL2020 <loc.fitness1@gmail.com>
2022-09-28 11:08:34 -0400

    changes

--- src/components/FighterContainer.js
+++ src/components/FighterContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import FighterList from './FighterList';
 
 class FighterContainer extends React.Component {
@@ -9,36 +10,20 @@ class FighterContainer extends React.Component {
       fighters: []
     };
   }
-
+ 
   componentDidMount() {
     fetch('http://localhost:3000/fighters')
   .then((response) => response.json())
   .then((data) => console.log(data));
-    //  fetch("http://localhost:3000/fighters")
-    //     .then((response) => {
-    //         console.log(response)
-    //         let data 
-    //         if (response.ok) {
-
-    //             data = response.json()
-    //             console.log(data)
-    //         }
-    //         return data;
-    //     }) 
-    //     .then((fighters) => {
-       
-    //         console.log(fighters, "fighters here")
-        
-        
-    // })
-
-      
   }
-
+  
   render() {
-    return <FighterList fighters={this.state.fighters} />
+    return <FighterList fighters={this.state.fighters}  />
+    
   }
+  
 }
+
 
 export default FighterContainer;
 
